b12397f3fb8573c89cea2bf867e28de897f0ced7
Hussien Alomari <hussienalomari7@gmail.com>
2025-09-09 03:07:35 +0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,20 +1,27 @@
 console.log('hi . this is first coode in java script');
 
+let test =20;
+var testt = 19 ;
+const testtt = 18; 
+
+console.log(test) // 20
+console.log(testt) // 19
+console.log(testtt) // 18
+
+if(testtt>testt)
+{
+    document.getElementById('a').style.backgroundColor='red'  ; // color shape = red 
+}
+else if(testt>test)
+{
+    document.getElementById('a').style.backgroundColor='green'  ; // color shape = red 
+}
+else
+{
+    document.getElementById('a').style.backgroundColor='blue'  ; 
+}
 
 
 
-function red(){    
-document.getElementById('a').style.backgroundColor='red' 
-};
 
-function green(){
-    document.getElementById('a').style.backgroundColor='green' 
-};
 
-function blue(){
-    document.getElementById('a').style.backgroundColor='blue' 
-};
-
-function black(){
-    document.getElementById('a').style.backgroundColor='black' 
-};
