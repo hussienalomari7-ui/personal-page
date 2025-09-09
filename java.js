30ca79ebde57569e19f9030f8b8eefd329fe6b3e
Hussien Alomari <hussienalomari7@gmail.com>
2025-09-10 01:20:41 +0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,46 +1,9 @@
-console.log('hi . this is first coode in java script');
 
 
+function print(){
 
-for(let a=0 ; a<3 ; a=a+1 ){
-    console.log('hellow') ; // we are going to print "hellow" three 
+    document.getElementById('b').style.backgroundColor='green' ;
+    document.getElementById('c').style.backgroundColor='black' ;
+    document.getElementById('a').style.backgroundColor='blue' ;
+  
 }
-
-
-
-let test =20;
-var testt = 19 ;
-const testtt = 18; 
-
-console.log(test) // 20
-console.log(testt) // 19
-console.log(testtt) // 18
-
-if(testtt>testt)
-{
-    document.getElementById('a').style.backgroundColor='red'  ; // color shape = red 
-}
-else if(testt>test)
-{
-    document.getElementById('a').style.backgroundColor='green'  ; // color shape = red 
-}
-else
-{
-    document.getElementById('a').style.backgroundColor='blue'  ; 
-}
-
-switch (testtt) {
-    case  20 :{document.getElementById('b').style.backgroundColor='blue'  ;  }  
-        break;
-    case  19 :{document.getElementById('b').style.backgroundColor='black'  ;  }  
-        break;
-    case  18 :{document.getElementById('b').style.backgroundColor='red'  ;  }  
-        break;
-
-    default :{ document.getElementById('b').style.backgroundColor='green'  ; }
-        break;
-}
-
-
-
-
